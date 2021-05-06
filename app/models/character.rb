9c39ecf0efbd5df4d733a615db395d60efb22a1c
Lucia diez <lupe.diez@gmail.com>
2021-05-06 14:51:31 -0300

    base controller

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,4 @@
 class Character < ApplicationRecord
   has_many :character_appearances, class_name: 'character_appearance', foreign_key: 'reference_id'
-  validates :name, :weight, :age, :history presence: true
+  validates :name, :weight, :age, :history, presence: true
 end
--- db/migrate/20210506172230_create_character_appearances.rb
+++ db/migrate/20210506172230_create_character_appearances.rb
@@ -1,9 +1,9 @@
 class CreateCharacterAppearances < ActiveRecord::Migration[6.1]
   def change
     create_table :character_appearances do |t|
-      t.reference :movie
-      t.reference :character
-      t.reference :serie
+      t.references :movie
+      t.references :character
+      t.references :serie
 
       t.timestamps
     end
