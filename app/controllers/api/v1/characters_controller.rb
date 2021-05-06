5ed20b4b0bc6e37c0cf9bcc501dc7336addf92b1
Lucia diez <lupe.diez@gmail.com>
2021-05-06 15:13:28 -0300

    series and character controllers and actions

---
+++ app/controllers/api/v1/characters_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::CharactersController < ApplicationController
+  before_action :set_character, only: %i[show create update delete]
+
+  def index
+    @characters = Character.all
+  end
+
+  def show 
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def update
+    @character = Character.update(character_params)
+    if @character.save
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def render_error
+    render json: { errors: @character.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :age, :weight, :history, :serie_id, :movie_id)
+  end
+end
+
+end
